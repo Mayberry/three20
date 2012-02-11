41b976b701e6ae09fdf8c2b731596bcf71a2cc32
Michael <bixby.sk@gmail.com>
2012-02-11 19:27:22 +0100

    Support for parameters in TTStyleSheet methods

--- src/Three20Style/Sources/TTStyleSheet.m
+++ src/Three20Style/Sources/TTStyleSheet.m
@@ -106,23 +106,33 @@ static TTStyleSheet* gStyleSheet = nil;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (TTStyle*)styleWithSelector:(NSString*)selector forState:(UIControlState)state {
-  NSString* key = state == UIControlStateNormal
-    ? selector
-    : [NSString stringWithFormat:@"%@%d", selector, state];
-  TTStyle* style = [_styles objectForKey:key];
-  if (!style) {
-    SEL sel = NSSelectorFromString(selector);
-    if ([self respondsToSelector:sel]) {
-      style = [self performSelector:sel withObject:(id)state];
-      if (style) {
-        if (!_styles) {
-          _styles = [[NSMutableDictionary alloc] init];
+    NSString* key = state == UIControlStateNormal
+        ? selector
+        : [NSString stringWithFormat:@"%@%d", selector, state];
+    TTStyle* style = [_styles objectForKey:key];
+    
+    NSArray *components = [selector componentsSeparatedByString:@"/"];
+    BOOL hasArguments = [components count] > 0;
+    
+    if (!style) {
+        SEL sel = (!hasArguments)
+        ? NSSelectorFromString(selector) 
+        : NSSelectorFromString([NSString stringWithFormat:@"%@:", [components objectAtIndex:0]]);
+        
+        if ([self respondsToSelector:sel]) {
+            style = (!hasArguments)
+            ? [self performSelector:sel withObject:(id)state] 
+            : [self performSelector:sel withObject:[components objectAtIndex:1]];
+            
+            if (style) {
+                if (!_styles) {
+                    _styles = [[NSMutableDictionary alloc] init];
+                }
+                [_styles setObject:style forKey:key];
+            }
         }
-        [_styles setObject:style forKey:key];
-      }
     }
-  }
-  return style;
+    return style;
 }
 
 
